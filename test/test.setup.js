55571f9f67f090d1943c373fdab23ee18eb5f9cc
Ilkka Lehtinen <ilkka.lehtinen@opuscapita.com>
2018-01-10 11:27:56 +0200

    Upgrade to react 15.6 & babel-preset-env

--- test/test.setup.js
+++ test/test.setup.js
@@ -1,7 +1,6 @@
-import chai from 'chai';
 import { JSDOM } from 'jsdom';
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15.4';
+import Adapter from 'enzyme-adapter-react-15';
 
 import mockStorage from './storage.mock';
 
