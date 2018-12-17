91e6d9e2459f047556cfd381cc17745752c0883a
Sami Karjalainen <ext-Sami.Karjalainen@opuscapita.com>
2018-12-17 15:29:10 +0200

    Upgrade to new component layout

--- test/icons/icon.component.spec.jsx
+++ test/icons/icon.component.spec.jsx
@@ -11,9 +11,7 @@ describe('Icon component', function describe() {
   it('should render correctly', () => {
     // This will only test the test stub defined in test.setup.js
     // Testing the real Icon component hasn't succeeded yet.
-    const wrapper = mount(
-      <Icon type="indicator" name="more" />,
-    );
+    const wrapper = mount(<Icon type="indicator" name="more" />);
     expect(wrapper.props()).to.eql({
       height: null,
       type: 'indicator',
